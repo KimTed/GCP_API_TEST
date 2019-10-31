04e8cc2d20a15eebf6dae81a99b53897b74dde99
KimTed <bbielo20@gmail.com>
2019-10-31 10:26:36 +0900

    feat: init project

---
+++ storage_sample.js
@@ -0,0 +1,26 @@
+'use strict'
+// Imports the Google Cloud client library.
+const {Storage} = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+async function main() {
+    const storage = new Storage({projectId: process.env.GCP_PROJECTID, keyFilename: prcess.env.CREDENTIAL_PATH});
+
+    try {
+    // Makes an authenticated API request.
+    const results = await storage.getBuckets();
+
+    const [buckets] = results;
+
+    console.log('Buckets:');
+    buckets.forEach(bucket => {
+        console.log(bucket.name);
+    });
+    } catch (err) {
+    console.error('ERROR:', err);
+    }
+}
+
+main().catch(console.error);
---
+++ video_intelligence_sample.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+'use strict';
+(async () => {
+  
+  const videoIntelligence = require('@google-cloud/video-intelligence');
+  const client = new videoIntelligence.VideoIntelligenceServiceClient({projectId: process.env.GCP_PROJECTID, keyFilename: prcess.env.CREDENTIAL_PATH});
+  const gcsUri = 'gs://comp_storage/Comp_video/main.mp4';
+  const request = {
+    inputUri: gcsUri,
+    features: ['LABEL_DETECTION'],
+  };
+  const [operation] = await client.annotateVideo(request);
+  console.log(
+    'Waiting for operation to complete... (this may take a few minutes)'
+  );
+  const [operationResult] = await operation.promise();
+  const annotations = operationResult.annotationResults[0];
+  const labels = annotations.segmentLabelAnnotations;
+  labels.forEach(label => {
+    console.log(`Label ${label.entity.description} occurs at:`);
+    label.segments.forEach(segment => {
+      segment = segment.segment;
+      if (segment.startTimeOffset.seconds === undefined) {
+        segment.startTimeOffset.seconds = 0;
+      }
+      if (segment.startTimeOffset.nanos === undefined) {
+        segment.startTimeOffset.nanos = 0;
+      }
+      if (segment.endTimeOffset.seconds === undefined) {
+        segment.endTimeOffset.seconds = 0;
+      }
+      if (segment.endTimeOffset.nanos === undefined) {
+        segment.endTimeOffset.nanos = 0;
+      }
+      console.log(
+        `Start: ${segment.startTimeOffset.seconds} +
+          .${(segment.startTimeOffset.nanos / 1e6).toFixed(0)}`
+      );
+      console.log(
+        `End: ${segment.endTimeOffset.seconds}. +
+          ${(segment.endTimeOffset.nanos / 1e6).toFixed(0)}`
+      );
+    });
+  });
+})();
